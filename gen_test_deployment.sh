d1c7b1d372c86df60e175a0372f0496ec5800e74
Citty <8428080+cittyinthecloud@users.noreply.github.com>
2025-02-17 01:41:06 -0500

    match new secrets location

--- gen_test_deployment.sh
+++ gen_test_deployment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p secrets
 rm secrets/secrets.json
+rm secrets/global.secrets
 rm subscription{1,2,3,4}.bin
-python -m ectf25_design.gen_secrets secrets/secrets.json 1 2 3 4
-python -m ectf25_design.gen_subscription secrets/secrets.json subscription1.bin 0xDEADBEEF 0 18446744073709551615 1
-python -m ectf25_design.gen_subscription secrets/secrets.json subscription2.bin 0xDEADBEEF 0 18446744073709551615 2
-python -m ectf25_design.gen_subscription secrets/secrets.json subscription3.bin 0xDEADBEEF 0 18446744073709551615 3
-python -m ectf25_design.gen_subscription secrets/secrets.json subscription4.bin 0xDEADBEEF 0 18446744073709551615 4
+python -m ectf25_design.gen_secrets secrets/global.secrets 1 2 3 4
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription1.bin 0xDEADBEEF 0 18446744073709551615 1
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription2.bin 0xDEADBEEF 0 18446744073709551615 2
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription3.bin 0xDEADBEEF 0 18446744073709551615 3
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription4.bin 0xDEADBEEF 0 18446744073709551615 4
