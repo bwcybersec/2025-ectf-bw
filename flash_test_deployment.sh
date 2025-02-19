98c4fc0c5cc76efa09dfc4be7ae3ea948c11f89a
Citty <8428080+cittyinthecloud@users.noreply.github.com>
2025-02-18 19:22:25 -0500

    fix building

--- flash_test_deployment.sh
+++ flash_test_deployment.sh
@@ -10,4 +10,8 @@ python -m ectf25.tv.subscribe subscription1.bin /dev/ttyACM0
 python -m ectf25.tv.subscribe subscription2.bin /dev/ttyACM0
 python -m ectf25.tv.subscribe subscription3.bin /dev/ttyACM0
 python -m ectf25.tv.subscribe subscription4.bin /dev/ttyACM0
+python -m ectf25.tv.subscribe subscription6.bin /dev/ttyACM0
+python -m ectf25.tv.subscribe subscription7.bin /dev/ttyACM0
+python -m ectf25.tv.subscribe subscription8.bin /dev/ttyACM0
+python -m ectf25.tv.subscribe subscription9.bin /dev/ttyACM0
 python -m ectf25.tv.list /dev/ttyACM0
