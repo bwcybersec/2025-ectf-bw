9b876694d20ed0889e93d40afbcd225b0668762d
Citty <8428080+cittyinthecloud@users.noreply.github.com>
2025-02-18 19:22:25 -0500

    fix building

--- decoder/build.sh
+++ decoder/build.sh
@@ -3,7 +3,8 @@
 
 export CARGO_TARGET_DIR=target_docker
 
-cargo build
+cargo clean
+cargo build --release
 rust-objcopy -O binary target_docker/thumbv7em-none-eabihf/release/decoder /out/max78000.bin
 cp target_docker/thumbv7em-none-eabihf/release/decoder /out/max78000.elf
 # cargo build --profile=release-with-debug
--- gen_test_deployment.sh
+++ gen_test_deployment.sh
@@ -2,9 +2,13 @@
 mkdir -p secrets
 rm secrets/secrets.json
 rm secrets/global.secrets
-rm subscription{1,2,3,4}.bin
-python -m ectf25_design.gen_secrets secrets/global.secrets 1 2 3 4
+rm subscription{1,2,3,4,6,7,8,9}.bin
+python -m ectf25_design.gen_secrets secrets/global.secrets 1 2 3 4 6 7 8 9
 python -m ectf25_design.gen_subscription secrets/global.secrets subscription1.bin 0xDEADBEEF 0 18446744073709551615 1
 python -m ectf25_design.gen_subscription secrets/global.secrets subscription2.bin 0xDEADBEEF 0 18446744073709551615 2
 python -m ectf25_design.gen_subscription secrets/global.secrets subscription3.bin 0xDEADBEEF 0 18446744073709551615 3
 python -m ectf25_design.gen_subscription secrets/global.secrets subscription4.bin 0xDEADBEEF 0 18446744073709551615 4
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription6.bin 0xDEADBEEF 2345345 18446744079551615 6
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription7.bin 0xDEADBEEF 645645 18446744070951615 7
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription8.bin 0xDEADBEEF 23452345 184463709551615 8
+python -m ectf25_design.gen_subscription secrets/global.secrets subscription9.bin 0xDEADBEEF 54345234 1873709551615 9
